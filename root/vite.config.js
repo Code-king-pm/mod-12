be9d20bf564ed9e21bcefc1b790a23f5e15888db
Code-king-pm <nathanchavers@gmail.com>
2025-02-09 15:18:33 -0600

    first iteration

--- root/vite.config.js
+++ root/vite.config.js
@@ -3,9 +3,17 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+
+  base: "/Code-king-pm/",
   plugins: [react()],
   server: {
     port: 3000,
     open: true,
   },
+
+
+
+   
+    
+  
 });
