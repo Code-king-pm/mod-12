63878156fb865e07a2082fd4dddc8f81c80fcc3b
Code-king-pm <nathanchavers@gmail.com>
2025-02-09 14:55:17 -0600

    first iteration

--- root/vite.config.js
+++ root/vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Code-king-pm/', // Change to match your repo name if using GitHub Pages
   server: {
     port: 3000,
     open: true,
